a5a5c1a2f440ed478ce38b9e8bac041fc560b549
Iryna Oliinyk <arairishka2@gmail.com>
2024-09-15 22:54:10 +0200

    alles + store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,47 @@
 import { configureStore } from '@reduxjs/toolkit';
 import filtersSlice from './filtersSlice';
 import contactsSlice from './contactsSlice';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistedContactsReducer = persistReducer(
+  {
+    key: 'user-contacts',
+    storage,
+    whitelist: ['items'],
+  },
+  contactsSlice,
+);
+
+const persistedFilterReducer = persistReducer(
+  {
+    key: 'user-filter',
+    storage,
+    whitelist: ['name'],
+  },
+  filtersSlice,
+);
 
 export const store = configureStore({
   reducer: {
-    contacts: contactsSlice,
-    filters: filtersSlice,
+    contacts: persistedContactsReducer,
+    filters: persistedFilterReducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
